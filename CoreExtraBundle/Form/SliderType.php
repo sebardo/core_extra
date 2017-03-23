37edf18f1d437d0bafe8c4573d4c15e42af8349c
sebardo <sebastian.sasturain@gmail.com>
2017-03-23 20:12:53 +0100

    More fixes

--- CoreExtraBundle/Form/SliderType.php
+++ CoreExtraBundle/Form/SliderType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use CoreBundle\Form\ImageType;
 
 /**
  * Class SliderType
